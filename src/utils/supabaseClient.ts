89a00f6deb23cb7c71a935caf1415bdb5e8733d7
qingbo <qingbo@yangrongsoft.com>
2024-01-31 17:08:42 +0800

    1

---
+++ src/utils/supabaseClient.ts
@@ -0,0 +1,9 @@
+// src/supabaseClient.js or .ts
+import { createClient } from '@supabase/supabase-js';
+console.log(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+ 
